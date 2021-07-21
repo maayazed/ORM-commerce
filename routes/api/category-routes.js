bfcc85df6dad0cfe75eefee0ed5beb9d71493b40
maayazed <mayahzebrasky@gmail.com>
2021-07-20 21:07:44 -0500

    Added all get requests for specified routes, started get by ID get for specfied routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,14 +3,36 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const findAll = await Category.findAll({
+      include: [{model: Product}],
+    });
+    res.status(200).json(findAll);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const findOne = await Category.findByPk(req.params.id, {
+      include: [{model: Product}],
+    });
+
+    if (!findOne) {
+      res.status(400).json({message: `No category found with id: ${req.params.id}!`});
+      return;
+    };
+
+    res.status(200).json(findOne);
+  } catch {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
